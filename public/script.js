c72ff441a1b9f13d3e3419c1b418899bc87551a1
Anny <quratulann_iqbal@hotmail.com>
2016-07-24 04:23:22 +0500

    improved color scheme

--- public/script.js
+++ public/script.js
@@ -1,16 +1,21 @@
-var job, newBox; //typescript ki file ext is .ts but yahan dom use krne k lie script must be in this file link ni krwa skti
+var job, newBox;
 function addTask() {
     job = document.getElementById("task").value; // input value assigned to variable 'job'
     newBox = "<tr>" +
         "<td>" + job + "</td>" +
-        // "<td> <button onclick='dlt(this);'>x</button> </td>" + // bootstrap ka danger button use kro circular ya cross wala glyphicon
-        "<td onclick='dlt(this);' style='border-radius:100%;'>X</td>" +
+        "<td>" +
+        "<button type='button' class='btn btn-danger' onclick='dlt(this);'>" +
+        "<span class='glyphicon glyphicon-trash' aria-hidden='true'></span>" +
+        "</button>" +
+        "</td>" +
         "</tr>"; // added new row to table with the input value
     document.getElementById("taskList").innerHTML += newBox; //adding new task to list
     document.getElementById("task").value = ''; // removing the filled text field
 }
 function dlt(z) {
-    //            alert(z.parentNode.nextSibling.innerHTML); // finally got it, the <td> to be dltd
+    //            alert(z.parentNode.innerHTML); // finally got it, the <td> to be dltd
     //            alert(z.parentNode.parentNode); //<tr> uska parent node mil gya
     z.parentNode.parentNode.innerHTML = ''; //yahoo urra diaaaa hahahaha
 }
+// "<td> <button onclick='dlt(this);'>x</button> </td>" + // bootstrap ka danger button use kro circular ya cross wala glyphicon
+//"<td onclick='dlt(this);'>X</td>" + 
--- public/script.ts
+++ public/script.ts
@@ -3,15 +3,21 @@ var job: string, newBox: string;
             job = document.getElementById("task").value; // input value assigned to variable 'job'
             newBox = "<tr>" +
                         "<td>" + job + "</td>" +
-                        // "<td> <button onclick='dlt(this);'>x</button> </td>" + // bootstrap ka danger button use kro circular ya cross wala glyphicon
-                        "<td onclick='dlt(this);' style='border-radius:100%;'>X</td>" +
-                    "</tr>";    // added new row to table with the input value
+                        "<td>" +
+                             "<button type='button' class='btn btn-danger' onclick='dlt(this);'>"+
+                                "<span class='glyphicon glyphicon-trash' aria-hidden='true'></span>" + //dlt button
+                             "</button>"+
+                         "</td>" +
+                      "</tr>";    // added new row to table with the input value
             document.getElementById("taskList").innerHTML += newBox; //adding new task to list
             document.getElementById("task").value = ''; // removing the filled text field
         }
 
         function dlt(z){
-//            alert(z.parentNode.nextSibling.innerHTML); // finally got it, the <td> to be dltd
+//            alert(z.parentNode.innerHTML); // finally got it, the <td> to be dltd
 //            alert(z.parentNode.parentNode); //<tr> uska parent node mil gya
             z.parentNode.parentNode.innerHTML = ''; //yahoo urra diaaaa hahahaha
         }
+
+        // "<td> <button onclick='dlt(this);'>x</button> </td>" + // bootstrap ka danger button use kro circular ya cross wala glyphicon
+                        //"<td onclick='dlt(this);'>X</td>" +
